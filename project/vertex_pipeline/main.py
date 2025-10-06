297fb3dd38044266530b71336ff7042bbf529711
RydlrCS <ted@rydlr.com>
2025-10-06 22:51:18 +0300

    commit 9e3818f22506b631434cd3b560bd6d3ff8284e18 Author: Rydlr <ted@rydlr.com>

--- project/vertex_pipeline/main.py
+++ project/vertex_pipeline/main.py
@@ -1,3 +1,7 @@
 # Entry point for GCP Cloud Function: trigger_vertex_pipeline
 # This file must be named main.py for Cloud Functions deployment
+
 from trigger_pipeline_function import http_trigger
+
+def main(request):
+	return http_trigger(request)
